eb62b8bcf4fc6778bf846e5215d117955237c2a9
ChapuisBenjamin <BenjaminChapuis27@gmail.com>
2026-02-03 10:53:45 +0100

    Ajout des icones panier et compte

--- nav.php
+++ nav.php
@@ -3,40 +3,48 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-
 $countPanier = 0;
 if (isset($_SESSION['panier'])) {
     foreach ($_SESSION['panier'] as $item) {
-        $countPanier += $item['q_plein'] + $item['q_edu'] + $item['q_kids'];
+        $countPanier += ($item['q_plein'] ?? 0) + ($item['q_edu'] ?? 0) + ($item['q_kids'] ?? 0);
     }
 }
+
+$userName = isset($_SESSION['user']) ? strtoupper(htmlspecialchars($_SESSION['user'])) : null;
 ?>
 
-<nav class="navbar">
-    <a href="panier.php" class="nav-cart">
-        PANIER
-        <?php if ($countPanier > 0): ?>
+<nav class="main-nav">
+  <ul class="menu">
+    <li class="menu-logo">
+      <a href="index.php" class="logo-link">
+        <img src="images/lumen logo.png" alt="Lumen Cinéma" class="logo-img">
+      </a>
+    </li>
+
+    <li><a href="index.php">Films</a></li>
+    <li><a href="cinema.php">Cinéma</a></li>
+    <li><a href="contact.php">Contact</a></li>
+    <li><a href="recherche.php">Recherche</a></li>
+
+    <li class="menu-spacer"></li>
+
+    <li class="menu-icon">
+      <a href="panier.php" class="icon-link" aria-label="Panier" title="Panier">
+        <span class="icon-wrap">
+          <img src="images/panier.png" alt="" class="nav-icon">
+          <?php if ($countPanier > 0): ?>
             <span class="cart-badge"><?php echo $countPanier; ?></span>
-        <?php endif; ?>
-    </a>
-</nav>
-<nav>
-    
-    <ul class="menu">
-        <li><img src="images/lumen logo.png"></li>
-        <li><a href="index.php">Films</a></li>
-        <li><a href="cinema.php">Cinéma</a></li>
-        <li><a href="contact.php">Contact</a></li>
-        <li><a class="nav-link nav-cart" href="panier.php">Panier <?php if ($countPanier > 0): ?><span class="cart-badge"><?php echo $countPanier; ?></span><?php endif; ?></a></li>
-        <li><a href="recherche.php">Recherche</a></li>
-       <li> <a href="compte.php" class="nav-user">
-            <?php if (isset($_SESSION['user'])): ?>
-                👤 <?php echo strtoupper(htmlspecialchars($_SESSION['user'])); ?>
-            <?php else: ?>
-                MON COMPTE
-            <?php endif; ?>
-        </a></li>
-    </ul>
-</nav>
+          <?php endif; ?>
+        </span>
+      </a>
+    </li>
 
-
+    <li class="menu-icon">
+      <a href="compte.php" class="icon-link" aria-label="Compte" title="<?php echo $userName ? $userName : 'Mon compte'; ?>">
+        <span class="icon-wrap">
+          <img src="images/compte.png" alt="" class="nav-icon">
+        </span>
+      </a>
+    </li>
+  </ul>
+</nav>
